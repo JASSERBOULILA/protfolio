75d79b722fa330c221f6b8da5af683299db78cfc
jasser boulila <jasserboulila@proton.me>
2026-01-29 17:53:05 +0100

    fixed the target model error

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,28 +1,50 @@
-import { useGLTF } from '@react-three/drei';
-import { useRef } from 'react';
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
-const Target = (props) => {
+export default function Target(props) {
   const targetRef = useRef();
-  const { scene } = useGLTF(
-    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
-  );
 
   useGSAP(() => {
+    if (!targetRef.current) return;
     gsap.to(targetRef.current.position, {
       y: targetRef.current.position.y + 0.5,
       duration: 1.5,
       repeat: -1,
       yoyo: true,
+      ease: "sine.inOut",
     });
-  });
+  }, []);
 
   return (
-    <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
-      <primitive object={scene} />
-    </mesh>
-  );
-};
+    <group {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
+      {/* Stand */}
+      <mesh position={[0, -0.6, 0]}>
+        <cylinderGeometry args={[0.05, 0.08, 1.2, 24]} />
+        <meshStandardMaterial color="#666" />
+      </mesh>
 
-export default Target;
+      {/* Base */}
+      <mesh position={[0, -1.25, 0]}>
+        <cylinderGeometry args={[0.35, 0.35, 0.1, 32]} />
+        <meshStandardMaterial color="#444" />
+      </mesh>
+
+      {/* Target disc */}
+      <mesh position={[0, 0.2, 0]}>
+        <cylinderGeometry args={[0.45, 0.45, 0.08, 48]} />
+        <meshStandardMaterial color="#f5f5f5" />
+      </mesh>
+
+      {/* Rings */}
+      <mesh position={[0, 0.25, 0.05]}>
+        <ringGeometry args={[0.28, 0.36, 64]} />
+        <meshStandardMaterial color="#6c63ff" side={2} />
+      </mesh>
+      <mesh position={[0, 0.25, 0.06]}>
+        <ringGeometry args={[0.14, 0.22, 64]} />
+        <meshStandardMaterial color="#ff4d4d" side={2} />
+      </mesh>
+    </group>
+  );
+}
